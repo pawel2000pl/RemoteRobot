03b07bcbff5696c9888208f6c9cf3ffecf48b902
Paweł Bielecki <pawelek24@op.pl>
2022-11-18 00:13:56 +0100

    Added QR scanner

--- install.sh
+++ install.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-sudo apt install python3 python3-pip
-sudo pip3 install opencv-python flask numpy pySerial
+sudo apt install -y python3 python3-pip libavcodec-extra
+sudo pip3 install opencv-python flask numpy pySerial simpleaudio
--- main.py
+++ main.py
@@ -37,13 +37,17 @@ class RobotServer:
     def __init__(self, port="/dev/ttyS0"):
         self.currentFrame = np.zeros([640,480,3])
         self.stopping = False
-        self.voltage = 0.0
+        self.frameCount = 0;
+        self.arduinoData = dict()
         self.updateTime = time()
         self.lastFrameTime = time()
         self.driving = {key: 0 for key in VALUES}
         self.serial = Serial(port, 115200)
-        self.arduinoThread = threading.Thread(target=lambda: self.arduinoLoop())
+        self.qrCodeDetector = cv2.QRCodeDetector()
+        self.arduinoThread = threading.Thread(target=self.arduinoLoop)
         self.arduinoThread.start()
+        self.QrThread = threading.Thread(target=self.detectQRLoop)
+        self.QrThread.start()
         self.startCameraThread()
         self.app = Flask("robot")
         self.app.route('/')(self.index)
@@ -55,7 +59,10 @@ class RobotServer:
         try:
             if self.cameraThread.is_alive():
                 self.cameraThread.join()
-            self.arduinoThread.join()
+            if self.QrThread.is_alive():
+                self.QrThread.join()
+            if self.arduinoThread.is_alive():
+                self.arduinoThread.join()
         except:
             pass
 
@@ -68,23 +75,46 @@ class RobotServer:
 
     def gen_frames(self):  
         while True:
-            myFrame = self.currentFrame[::2, ::2, :]
-            a, b = np.quantile(myFrame, [0.05, 0.95])
-            myFrame = (256/(b-a+1)) * (myFrame-a)
-            _ret, buffer = cv2.imencode('.jpg', myFrame, [int(cv2.IMWRITE_JPEG_QUALITY), 42])
+            _ret, buffer = cv2.imencode('.jpg', self.currentFrame, [int(cv2.IMWRITE_JPEG_QUALITY), 42])
             frame = buffer.tobytes()
             self.lastFrameTime = time()
             yield (b'--frame\r\n' + b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')  
             sleep(1/30)
 
+    def detectQRLoop(self):
+        currentFrameNumber = 0
+        TARGET = "target2"
+        while not self.stopping:
+            sleep(0.2)
+            if currentFrameNumber != self.frameCount:
+                currentFrameNumber = self.frameCount
+                anything, decodedText, points, _ = self.qrCodeDetector.detectAndDecodeMulti(self.currentFrame)
+                if not anything:
+                    continue
+                results = [r for r in zip(decodedText, points) if r[0] == TARGET]
+                if len(results) != 1:
+                    continue
+                decodedText, points = results[0]
+                points = [(float(points[i, 0]), float(points[i, 1])) for i in range(4)]
+                shape = self.currentFrame.shape
+                # print([(p[0]/shape[0], p[1]/shape[1]) for p in points])
+
+
     def cameraLoop(self):    
         self.lastFrameTime = time()
         camera = cv2.VideoCapture(0)
+        camera.set(cv2.CAP_PROP_FPS, 30)
         try:
             while (not self.stopping) and (time() - self.lastFrameTime < 3.0):
                 ret, newframe = camera.read()
-                if ret:
-                    self.currentFrame = newframe
+                if ret:            
+                    newframe = newframe[::2, ::2, :]
+                    a, b = np.quantile(newframe, [0.05, 0.95])
+                    newframe = (255/(b-a+1)) * (newframe-a)
+                    newframe[newframe>255] = 255
+                    newframe[newframe<0] = 0
+                    self.currentFrame = newframe.astype(np.uint8)
+                    self.frameCount += 1
         finally:        
             camera.release()
 
@@ -132,8 +162,14 @@ class RobotServer:
             self.sendMessage(ID_GRAB, self.driving["grab"])
             self.sendMessage(ID_MAX_ENGINE_CORRECTION, self.driving["maxEngineCorrection"])
 
-            while self.serial.in_waiting:
-                self.voltage = int(self.serial.read(1)[0])*5/255
+            try:
+                while self.serial.in_waiting > 64:
+                    line = self.serial.read_until(b'\n').decode(encoding="utf-8").strip()
+                    if len(line)>2:
+                        self.arduinoData = json.loads(line)
+            except Exception as err:
+                print("Error during recieving data from arduino:", err)
+                print(line)
             
             sleep(0.03)
 
@@ -146,15 +182,18 @@ class RobotServer:
         return Response(self.gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
     def setValue(self):
+        dataToSend = self.arduinoData.copy()
         try:
             data = request.json
             for name in set(self.driving.keys()).intersection(set(data.keys())):
                 self.driving[name] = data[name]
             self.updateTime = time()
-            return json.dumps({"status": "ok", "voltage": self.voltage})
+            dataToSend["status"] = "ok"
+            return json.dumps(dataToSend)
         except Exception as err:
             print(err)
-            return json.dumps({"status": "error", "voltage": self.voltage})
+            dataToSend["status"] = "error"
+            return json.dumps(dataToSend)
 
     def run(self):
         self.app.run("0.0.0.0")
